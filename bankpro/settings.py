4a9ab316f045af5cede4ec42b075bf212810d511
GhGuda <toyboipressure1@gmail.com>
2024-09-11 14:24:04 +0000

    Add gunicorn to requirements

--- bankpro/settings.py
+++ bankpro/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-l%milsflts!0gs)8hic#=*wn5x_tg&+*cs2f@3c@&6@$m%&1kl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://bank-ribq.onrender.com"]
+ALLOWED_HOSTS = ["bank-ribq.onrender.com"]
 
 
 # Application definition
